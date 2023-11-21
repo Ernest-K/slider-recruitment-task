4661b23e7581542b0d30eb257a88d83eb8950d23
Ernest K <ernio789@gmail.com>
2023-11-22 00:10:24 +0100

    create slider control component

---
+++ src/components/SliderControl/SliderControl.tsx
@@ -0,0 +1,42 @@
+import ArrowLeftIcon from "../../assets/icons/Arrow-Left.svg";
+import ArrowRightIcon from "../../assets/icons/Arrow-Right.svg";
+import "../IconButton/IconButton.css";
+import "./SliderControl.css";
+import "../../index.css";
+import IconButton from "../IconButton/IconButton";
+
+interface NavigationProps {
+  onPrev: () => void;
+  onNext: () => void;
+  active: number;
+  total: number;
+}
+
+export default function SliderControl({
+  onPrev,
+  onNext,
+  active,
+  total,
+}: NavigationProps) {
+  return (
+    <div className="control__container" data-testid="slider-control">
+      <IconButton
+        onClick={onPrev}
+        color="#FFA6F6"
+        iconSrc={ArrowLeftIcon}
+        iconStyle={{ width: 48 }}
+      />
+      <div className="control__counter" data-testid="slider-counter">
+        <p>
+          <span>{active}</span>/<span>{total}</span>
+        </p>
+      </div>
+      <IconButton
+        onClick={onNext}
+        color="#A6FAFF"
+        iconSrc={ArrowRightIcon}
+        iconStyle={{ width: 48 }}
+      />
+    </div>
+  );
+}
