a7ab182a967ed95704280f26be1f780382e4e867
Ernest K <ernio789@gmail.com>
2023-11-22 00:17:54 +0100

    create slider wrapper component

---
+++ src/components/SliderWrapper/SliderWrapper.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { SlideData } from "../../types";
+import Slider from "../Slider/Slider";
+import "../Slider/Slider.css";
+import getSlidesData from "../../services/slideService";
+
+export default function SliderWrapper() {
+  const [slidesData, setSlidesData] = useState<SlideData[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const fetchSlides = async () => {
+      setIsLoading(true);
+
+      try {
+        const slides = await getSlidesData();
+        setSlidesData(slides);
+      } catch (error: any) {
+        console.error("Error fetching slide data: ", error);
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchSlides();
+  }, []);
+
+  if (error) {
+    return <p>{`Something went wrong`}</p>;
+  }
+
+  return (
+    <section className="slider__wrapper" data-testid="slider-wrapper">
+      {isLoading && <p className="slider__loading">Loading</p>}
+      {slidesData.length && <Slider slides={slidesData} data-testid="slider" />}
+    </section>
+  );
+}
